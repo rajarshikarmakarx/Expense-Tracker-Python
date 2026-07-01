1642154fcd5ffef542326d51bfa3b7aa3bdd07fd
Rajarshi Karmakar <rajarshikarmakar123@gmail.com>
2026-07-01 23:22:45 +0530

    Created the Skeleton of the project

---
+++ expense_tracker.py
@@ -0,0 +1,21 @@
+from expense import Expense
+
+def main():
+    pass
+
+# Add A  New Entry
+def getNewEntry():
+    pass
+# Save the Entry
+def saveNewEntry():
+    pass
+# View all Entries
+def displayEntries():
+    pass
+
+
+
+if __name__=="__main__":
+    main()
+
+
